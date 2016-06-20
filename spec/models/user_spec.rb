48c5213e38fd6ffa3767414154238095a4b97516
Hector Mg <hectormgwake@gmail.com>
2016-06-20 12:38:44 -0500

    Add user first specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = FactoryGirl.build :user }
+
+  describe "user" do
+    it "should respond to attributes" do
+      expect(@user).to respond_to(:email)
+      expect(@user).to respond_to(:password)
+      expect(@user).to respond_to(:password_confirmation)
+    end
+
+    it "should be valid" do
+      expect(@user).to be_valid
+    end
+  end
 end
