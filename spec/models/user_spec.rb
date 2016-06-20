a4eb9e358b6586594561e6ce0930ab623844b17c
Hector Mg <hectormgwake@gmail.com>
2016-06-20 14:00:36 -0500

    Adds shoulda matchers for test refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { should validate_confirmation_of(:password) }
+    it { should allow_value('example@domain.com').for(:email) }
+  end
 end
