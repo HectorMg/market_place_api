86016d84071ca273f3acbb0397b7ce387cb25a90
Hector Mg <hectormgwake@gmail.com>
2016-06-22 13:02:09 -0500

    Adds user order has many relation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,7 +23,8 @@ RSpec.describe User, type: :model do
     it { should validate_confirmation_of(:password) }
     it { should allow_value('example@domain.com').for(:email) }
     it { should validate_uniqueness_of(:auth_token)}
-    it { should have_many(:products)}
+    it { should have_many :products}
+    it { should have_many :orders }
   end
 
   describe "#generate_authentication_token!" do
