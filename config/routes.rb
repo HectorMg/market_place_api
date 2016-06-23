9e0e4a2f4ade50650cab0a752649b8285109a201
Hector Mg <hectormgwake@gmail.com>
2016-06-22 22:41:25 -0500

    Adds the create method for the orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ MarketPlaceApi::Application.routes.draw do
       end
       resources :sessions, :only => [:create, :destroy]
       resources :products, :only => [:show, :index]
-      resources :orders, :only => [:index, :show]
+      resources :orders, :only => [:index, :show, :create]
     end
   end
 end
